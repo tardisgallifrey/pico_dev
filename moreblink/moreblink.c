bdb32325d0be19cf331dbdef916cfc52ef848cdf
Dave <bigmanrunning@gmail.com>
2022-01-01 13:46:52 -0600

    moreblink 01/01/2022

---
+++ moreblink/moreblink.c
@@ -0,0 +1,30 @@
+/**
+* Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#include "pico/stdlib.h"
+
+int main() {
+   #ifndef PICO_DEFAULT_LED_PIN
+   #warning blink example requires a board with a regular LED
+   #else
+   const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+   const uint BLUE_LED = 20;
+
+   gpio_init(LED_PIN);
+   gpio_set_dir(LED_PIN, GPIO_OUT);
+   gpio_init(BLUE_LED);
+   gpio_set_dir(BLUE_LED, GPIO_OUT);
+
+   while (true) {
+      gpio_put(LED_PIN, 1);
+      gpio_put(BLUE_LED, 0);
+      sleep_ms(250);
+      gpio_put(LED_PIN, 0);
+      gpio_put(BLUE_LED, 1);
+      sleep_ms(250);
+   }
+   #endif
+}
